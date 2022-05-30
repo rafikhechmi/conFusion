af96ba1e84901bcf9b86fe4143c644c8df7eb395
rafik <rafikhechmi@gmail.com>
2022-05-30 23:58:40 +0100

    Single Page Applications Part 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {MenuComponent} from "./menu/menu.component";
 import {ContactComponent} from "./contact/contact.component";
+import {DishdetailComponent} from "./dishdetail/dishdetail.component";
 
 const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
+  { path: 'dishdetail/:id',     component: DishdetailComponent },
   { path: 'contactus',     component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 
