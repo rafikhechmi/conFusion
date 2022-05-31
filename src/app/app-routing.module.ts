2be0041718be966625105bbbc61095ca4f4a55fe
rafik <rafikhechmi@gmail.com>
2022-05-31 15:34:47 +0100

    Single Page Applications Part 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import {HomeComponent} from "./home/home.component";
 import {MenuComponent} from "./menu/menu.component";
 import {ContactComponent} from "./contact/contact.component";
 import {DishdetailComponent} from "./dishdetail/dishdetail.component";
+import {AboutComponent} from "./about/about.component";
 
 const routes: Routes = [
   { path: 'home',  component: HomeComponent },
+  { path: 'about',  component: AboutComponent },
   { path: 'menu',     component: MenuComponent },
   { path: 'dishdetail/:id',     component: DishdetailComponent },
   { path: 'contactus',     component: ContactComponent },
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import {DISHES} from "../shared/dishes";
 import {DishService} from "../services/dish.service";
+import {LeaderService} from "../services/leader.service";
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -12,10 +13,12 @@ export class MenuComponent implements OnInit {
   dishes: Dish[];
 
   selectedDish: Dish;
-  constructor(private dishService: DishService) { }
+  constructor(private dishService: DishService ) { }
 
   ngOnInit(): void {
     this.dishes = this.dishService.getDishes();
+
+
   }
 
   onSelect(dish: Dish) {
